1d06c19319a0bb2d5b5c82d631dadf32cc6c41c9
janus_wel <janus.wel.3@gmail.com>
2015-12-07 14:59:51 +0900

    add shell script to run influxdb container

---
+++ influxdb/influxdb.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+docker run	\
+	-d	\
+	--name influxdb	\
+	-v ./influxdb.conf:/etc/influxdb/influxdb.conf	\
+	-v /var/lib/influxdb/data	\
+	-p 8083:8083	\
+	-p 8086:8086	\
+	-p 8088:8088	\
+	--restart always	\
+	januswel/influxdb
